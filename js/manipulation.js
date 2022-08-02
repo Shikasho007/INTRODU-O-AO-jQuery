bd3857fa117c941f069fc707d2733e11785323c6
Shikasho007 <shikashogamer007@gmail.com>
2022-08-02 19:05:58 -0300

    Aula03/08

--- js/manipulation.js
+++ js/manipulation.js
@@ -1,18 +1,35 @@
-$(document).ready(function(){
+$(document).ready(function() {
     //As manipulções que eu desejar devem estar dentro deste bloco
-    $('#btn-1-5em').click(function(){
+    $('#btn-1-5em').click(function() {
         $('p').removeClass('font-2-0');
         $('p').removeClass('font-2-5');
         $('p').addClass('font-1-5');
     });
-    $('#btn-2-0em').click(function(){
+    $('#btn-2-0em').click(function() {
         $('p').removeClass('font-1-5');
         $('p').removeClass('font-2-5');
         $('p').addClass('font-2-0');
     });
-    $('#btn-2-5em').click(function(){
+    $('#btn-2-5em').click(function() {
         $('p').removeClass('font-1-5');
         $('p').removeClass('font-2-0');
         $('p').addClass('font-2-5');
     });
+
+    // Funções para manipulções das imagens
+    $('#btn-hide').click(function() {
+        $('.image').fadeOut('slow');
+        $('.menu').fadeToggle('slow');
+    });
+    $('#btn-show').click(function() {
+        $('.image').fadeIn('slow');
+        $('.menu').fadeToggle('slow');
+    });
+
+    // Iniciando o meu menu escondido
+    $('.menu').hide();
+
+    $('.nav-bar').click(function() {
+        $('.menu').fadeToggle('slow');
+    })
 });
